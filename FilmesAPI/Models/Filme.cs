f7f1b0ad37ca14108969917be9b61842500bd6b8
MarioLuiz <marioluiz.arruda@gmail.com>
2023-06-15 18:57:56 -0400

    feat: validacao dos campos do filme

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,10 +1,18 @@
-﻿namespace FilmesAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
 
 public class Filme
 {
+    [Required(ErrorMessage = "O titulo é obrigatório")]
+    [MaxLength(100, ErrorMessage = "O tamanho do titulo não pode exeder 100 caracteres")]
     public string Titulo { get; set; }
 
+    [Required(ErrorMessage = "O genero é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do genero não pode exeder 50 caracteres")]
     public string Genero { get; set; }
-
+    
+    [Required(ErrorMessage = "A duração é obrigatória")]
+    [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 a 600 minutos")]
     public int Duracao { get; set; }
 }
