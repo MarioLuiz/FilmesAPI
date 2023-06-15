25321d47c785f7ca4420db1470a683c8e2c0b02f
MarioLuiz <marioluiz.arruda@gmail.com>
2023-06-15 17:26:21 -0400

    Estrutura inicial da API

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+     public void AdicionarFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine("Titulo do Filme: " + filme.Titulo);
+        Console.WriteLine("Genero do Filme: " + filme.Genero);
+        Console.WriteLine("Duração do Filme: " + filme.Duracao);
+    }
+}
