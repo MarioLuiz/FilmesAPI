3e9f67a6da63a0d99f21195736a64e18f2fc256e
MarioLuiz <marioluiz.arruda@gmail.com>
2023-07-27 16:48:54 -0400

    feat: retorno dto read filme e metodo delete filme

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -4,6 +4,7 @@ using FilmesAPI.Data.Dtos;
 using FilmesAPI.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
 namespace FilmesAPI.Controllers;
@@ -31,9 +32,9 @@ public class FilmeController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RetornaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    public IEnumerable<ReadFilmeDto> RetornaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        return _context.filmes.Skip(skip).Take(take);
+        return _mapper.Map<List<ReadFilmeDto>>(_context.filmes.Skip(skip).Take(take));
     }
 
     [HttpGet("{id}")]
@@ -41,7 +42,8 @@ public class FilmeController : ControllerBase
     {
         var filme = _context.filmes.FirstOrDefault(filme => filme.Id == id);
         if (filme == null) return NotFound();
-        return Ok(filme);
+        var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+        return Ok(filmeDto);
     }
 
     [HttpPut("{id}")]
@@ -70,6 +72,18 @@ public class FilmeController : ControllerBase
         }
         _mapper.Map(filmeParaAtualizar, filme);
         _context.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeletaFilme(int id)
+    {
+        var filme = _context.filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+
+        _context.Remove(filme);
+        _context.SaveChanges();
+
         return NoContent();
     }
 
